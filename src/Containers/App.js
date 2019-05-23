a6a18f103471140eb6e57493541ecd13a738c2a0
Hanna Yang <hannayangyang@gmail.com>
2019-05-23 20:56:52 +0800

    still two bugs

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,77 +1,160 @@
 import React, { Component } from 'react';
 import './App.css';
-import Quote from '../Components/Quote/Quote'; 
-import Buttons from '../Components/Buttons/Buttons'; 
 
-const quoteLibrary = [
-  {text: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.", author: 'Dr. Suess', color: '#16a085'}, 
-  {text: 'Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.', author: 'Mark Twain', color: '#27ae60'}, 
-  {text: 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.', author: 'Mark Caine', color: '#2c3e50'}, 
-  {text: 'Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.', author: 'Lucille Ball', color: '#f39c12'}, 
-  {text: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.', author: 'Joshua J. Marine', color: '#e74c3c'},  
-  {text: 'Remember that the happiest people are not those getting more, but those giving more.', author: 'H. Jackson Brown, Jr.', color: '#9b59b6'}, 
-  {text: 'Live in the sunshine, swim the sea, drink the wild air.', author: 'Ralph Waldo Emerson', color: '#fb6964'},  
-  {text: 'We are what we repeatedly do; excellence, then, is not an act but a habit.', author: 'Aristotle', color: '#388894'}, 
-  {text: 'A man is a success if he gets up in the morning and gets to bed at night, and in between he does what he wants to do.', author: 'Bob Dylan', color: '#472e32'}, 
-  {text: 'The big lesson in life, baby, is never be scared of anyone or anything.', author: 'Frank Sinatra', color: '#94386C'}, 
-]; 
-
-const nextIndex = () => {
-  return Math.floor(Math.random() * quoteLibrary.length);
-};
-
-const constructTwitterUrl = {
-  preface: 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="', 
-  connection: '"', 
-  ending: '' }; 
-
-const constructTumblrUrl = {
-  preface: 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=', 
-  connection: '&content=', 
-  ending: '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
-};
+const isOperator = /[+-/*]/; 
+const floatDecimalSolution = (num) => {
+  return (Math.round(1000000000000 * num)) / 1000000000000
+}; 
 
 class App extends Component {
-  state = {
-    randomIndex: nextIndex(),
-  }; 
+  constructor(props) {
+    super(props); 
+    this.state = {
+      inputs: [], 
+      incompleteInput: '',
+    }
+    this.resetHandler = this.resetHandler.bind(this); 
+    // this.maxDigitWarning = this.maxDigitWarning.bind(this); 
+    this.numberHandler = this.numberHandler.bind(this); 
+    this.operatorHandler = this.operatorHandler.bind(this); 
+    this.decimalHandler = this.decimalHandler.bind(this); 
+    this.evalHandler = this.evalHandler.bind(this); 
+  }
 
-  clickHandler = ( ) => {
-    this.setState ({
-      randomIndex: nextIndex(), 
-    }); 
-  };
+  resetHandler() {
+    this.setState({
+      inputs: [],
+      incompleteInput: '',
+    })
+  }
+
+  // maxDigitWarning() {
+  //   this.setState({
+  //     display: 'DIGIT LIMIT MET'
+  //   }); 
+  //   setTimeout(() => this.setState({display: this.state.incompleteInput}), 1000);
+  // }
+
+  numberHandler(event) {
+    if(this.state.inputs.indexOf('=') === -1) {
+      this.setState({
+        incompleteInput: this.state.incompleteInput === '0' ? '0' : this.state.incompleteInput + (event.target.value + ''),
+      })
+    } else {
+      this.setState({
+        inputs: [], 
+        incompleteInput: event.target.value, 
+      })
+    }
+  }
+
+  operatorHandler(event) {
+    if(this.state.inputs.indexOf('=') === -1) {
+      this.setState({
+        inputs: this.state.inputs.concat([this.state.incompleteInput, event.target.value]), 
+        incompleteInput: '', 
+      })
+    } else {
+      this.setState({
+        inputs: [this.state.inputs[this.state.inputs.length - 1], event.target.value], 
+        incompleteInput: '', 
+      })
+    }
+  }
+
+  decimalHandler(event) {
+    if(this.state.inputs.indexOf('=') === -1) {
+      this.setState({
+        incompleteInput: this.state.incompleteInput.indexOf('.') === -1 ? this.state.incompleteInput + (event.target.value + '') : this.state.incompleteInput,
+    })
+  } else {
+    this.setState({
+      inputs: [], 
+      incompleteInput: event.target.value,
+      })
+    }
+  }
+
+  evalHandler(event) {
+    if(this.state.inputs.length === 0) {
+      if(this.state.incompleteInput !== '') {
+        this.setState({
+          inputs: [this.state.incompleteInput, event.target.value, floatDecimalSolution(eval(this.state.incompleteInput))], 
+          incompleteInput: floatDecimalSolution(eval(this.state.incompleteInput)), 
+        })
+      }
+    } else if(this.state.inputs.length === 1 && isOperator.test(this.state.inputs[0]) === true && this.state.incompleteInput === '') {
+        this.setState({
+          inputs: [], 
+          incompleteInput: '',
+        })
+    } else if(this.state.inputs.length >= 2 && isOperator.test(this.state.inputs[this.state.inputs.length - 1]) === true && this.state.inputs.indexOf('=') === -1) {
+      if(this.state.incompleteInput === '') {
+        this.setState({
+          inputs: [this.state.inputs.slice(0, this.state.inputs.length - 1).join(''), event.target.value, floatDecimalSolution(eval(this.state.inputs.slice(0, this.state.inputs.length -1).join('')))], 
+          incompleteInput: floatDecimalSolution(eval(this.state.inputs.slice(0, this.state.inputs.length -1).join(''))), 
+        })
+      } else {
+        this.setState({
+          inputs: [this.state.inputs.join(''), this.state.incompleteInput, event.target.value, floatDecimalSolution(eval(this.state.inputs.concat(this.state.incompleteInput).join('')))], 
+          incompleteInput: floatDecimalSolution(eval(this.state.inputs.concat(this.state.incompleteInput).join(''))), 
+        })
+      }
+    } else if(this.state.inputs.indexOf('=') >= 0) {
+      this.setState({
+        inputs: this.state.inputs, 
+        incompleteInput: this.state.incompleteInput,
+      })
+    }
+  }
+
+  getExtendedState(state) { 
+    return {
+      formularDisplay: state.inputs.join(''),
+      inputDisplay: state.incompleteInput,
+    };
+  }
 
   render () {
-    const quoteDisplay = quoteLibrary[this.state.randomIndex]; 
-
-    const fontColorStyle = {
-      color: quoteDisplay.color
-    }; 
-
-    const backgroundStyle = {
-      backgroundColor: quoteDisplay.color
-    }; 
-
-    const twitterFullUrl = () => {
-      return constructTwitterUrl.preface + quoteDisplay.text + constructTwitterUrl.connection + quoteDisplay.author
-    }; 
-
-    const tublrFullUrl = () => {
-      return constructTumblrUrl.preface + quoteDisplay.author + constructTumblrUrl.connection + quoteDisplay.text + constructTumblrUrl.ending
-    }; 
-
+    const extendedState = this.getExtendedState(this.state);
     return (
-      <div className="App" style = {backgroundStyle}>
-        <div style = {fontColorStyle}> 
-          <Quote 
-            text = {quoteDisplay.text}
-            author = {quoteDisplay.author}/>
-          <Buttons
-            twitterFullUrl = {twitterFullUrl()}
-            tublrFullUrl = {tublrFullUrl()}
-            clicked = {this.clickHandler}
-            buttonStyle = {backgroundStyle} />
+      <div className='wrap'>
+        <div className="calculator">
+          <div id='formula'>{extendedState.formularDisplay}</div>
+          <div id='input'>{extendedState.inputDisplay}</div>
+          <div className='buttons'>
+            <div className='row'>
+              <button id='clear'    className='big-buttons'       value='AC'  onClick={this.resetHandler}>A C</button>
+              <button id='divide'   className='operator-buttons'  value='/'   onClick={this.operatorHandler}>/</button>
+            </div>
+            <div className='row'>
+              <button id='seven'    className='number-buttons'    value='7'   onClick={this.numberHandler}>7</button>
+              <button id='eight'    className='number-buttons'    value='8'   onClick={this.numberHandler}>8</button>
+              <button id='nine'     className='number-buttons'    value='9'   onClick={this.numberHandler}>9</button>
+              <button id='multiply' className='operator-buttons'  value='*'   onClick={this.operatorHandler}>x</button>
+            </div>
+            <div className='row'>
+              <button id='four'     className='number-buttons'    value='4'   onClick={this.numberHandler}>4</button>
+              <button id='five'     className='number-buttons'    value='5'   onClick={this.numberHandler}>5</button>
+              <button id='six'      className='number-buttons'    value='6'   onClick={this.numberHandler}>6</button>
+              <button id='minus'    className='operator-buttons'  value='-'   onClick={this.operatorHandler}>-</button>
+            </div>
+            <div className='row'>
+              <button id='one'      className='number-buttons'    value='1'   onClick={this.numberHandler}>1</button>
+              <button id='two'      className='number-buttons'    value='2'   onClick={this.numberHandler}>2</button>
+              <button id='three'    className='number-buttons'    value='3'   onClick={this.numberHandler}>3</button>
+              <button id='add'      className='operator-buttons'  value='+'   onClick={this.operatorHandler}>+</button>
+            </div>
+            <div className='row'>
+              <button id='zero'     className='middle-buttons'    value='0'   onClick={this.numberHandler}>0</button>
+              <button id='decimal'  className='number-buttons'    value='.'   onClick={this.decimalHandler}>.</button>
+              <button id='equals'   className='equal-buttons'     value='='   onClick={this.evalHandler}>=</button>
+            </div>
+          </div>
+        </div>
+        <div className='footnote'>
+          <p> Designed and coded by </p>
+          <p> Hanna Yang </p>
         </div>
       </div>
     );
