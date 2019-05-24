7cb161bb0e8e0c0ab6660c90035bab1f24c2b7b5
Hanna Yang <hannayangyang@gmail.com>
2019-05-24 15:40:01 +0800

    added limit warning and did test case

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 const isOperator = /[+-/*]/; 
+// text cases: 3 * 0.6 = ; 0.1 + 0.2 = 
 const floatDecimalSolution = (num) => {
   return (Math.round(1000000000000 * num)) / 1000000000000
+}; 
+const result = (formularStr) => {
+  return eval(formularStr)
 }; 
 
 class App extends Component {
@@ -12,9 +16,9 @@ class App extends Component {
     this.state = {
       inputs: [], 
       incompleteInput: '',
+      warning: '', 
     }
     this.resetHandler = this.resetHandler.bind(this); 
-    // this.maxDigitWarning = this.maxDigitWarning.bind(this); 
     this.numberHandler = this.numberHandler.bind(this); 
     this.operatorHandler = this.operatorHandler.bind(this); 
     this.decimalHandler = this.decimalHandler.bind(this); 
@@ -28,19 +32,20 @@ class App extends Component {
     })
   }
 
-  // maxDigitWarning() {
-  //   this.setState({
-  //     display: 'DIGIT LIMIT MET'
-  //   }); 
-  //   setTimeout(() => this.setState({display: this.state.incompleteInput}), 1000);
-  // }
-
   numberHandler(event) {
-    if(this.state.inputs.indexOf('=') === -1) {
+    //test case: 999999999999999999 => If number string has 15 or more digits, then display a warning message, with a timeout of 1s.
+    if(this.state.incompleteInput.length >= 15){
+      this.setState({
+        warning: 'DIGIT LIMIT MET', 
+      }); 
+      setTimeout(() => this.setState({warning: ''}), 1000);
+    //test case: 3+000 => If number string has less than 15 digits: inputs: ['3', '+'] => if you already pressed a '0' for incompleteInput, then it can't be followed with another '0'. It can actually only be followed by a '.'.
+    } else if(this.state.incompleteInput.length < 15 && this.state.inputs.indexOf('=') === -1) {
       this.setState({
         incompleteInput: this.state.incompleteInput === '0' ? '0' : this.state.incompleteInput + (event.target.value + ''),
       })
-    } else {
+    //test case: 3+4=1 => inputs: ['3', '+', '4', '=' '7'] => A new calculation will be started and the inputs will become empty again. 
+    } else if(this.state.inputs.indexOf('=') !== -1){
       this.setState({
         inputs: [], 
         incompleteInput: event.target.value, 
@@ -49,17 +54,28 @@ class App extends Component {
   }
 
   operatorHandler(event) {
+    //test case: / => inputs: [], incompleteInput: ''; => If nothing has been pressed, then a new start should never be an operator. 
     if(this.state.incompleteInput === '' && this.state.inputs.length === 0) {
-    } else if(this.state.incompleteInput === '' && isOperator.test(this.state.inputs[this.state.inputs.length - 1]) === true) {
-      this.setState({
-        inputs: this.state.inputs.slice(0, this.state.inputs.length - 1).concat([event.target.value]), 
-        incompleteInput: '', 
-      })
-    } else if(this.state.inputs.indexOf('=') === -1) {
-      this.setState({
-        inputs: this.state.inputs.concat([this.state.incompleteInput, event.target.value]), 
-        incompleteInput: '', 
-      })
+    } 
+    //test case: ./ => No matter what is in the inputs array, an incomplete input like './' (a dot followed by an ooperator) should never be allowed. 
+    else if(this.state.incompleteInput === '.'){
+    } 
+    //if inputs: ['3'] or ['3', '+', '4'] (i.e. no '='), 
+    else if(this.state.inputs.indexOf('=') === -1) {
+    //test case: 3/+ => further if inputs: ['3', '/'] (i.e. ends with an operator) and incompleteInput is empty, then when a new operator is pressed, the new operator should override the previous operator. 
+      if(this.state.incompleteInput === '' && isOperator.test(this.state.inputs[this.state.inputs.length - 1]) === true){
+        this.setState({
+          inputs: this.state.inputs.slice(0, this.state.inputs.length - 1).concat([event.target.value]), 
+          incompleteInput: '', 
+        })
+    //test case: 3+4/ => if inputs doesn't include '=', and further, inputs doesn't end with an operator, e.g.: ['3', '+', '4'], then when a new operator is pressed, it should be pushed into the inputs and become part of the calculator, e.g.: ['3', '+', '4', '/']  
+      } else {
+        this.setState({
+          inputs: this.state.inputs.concat([this.state.incompleteInput, event.target.value]), 
+          incompleteInput: '', 
+        })
+      }
+    //test case: 3+4=/ => if inputs has '=' in it, e.g. inputs: ['3', '+', '4', '=', '7'], then when an operator is pressed, only the end value (i.e. '7') will be maintained in inputs for the next round of calculation. 
     } else {
       this.setState({
         inputs: [this.state.inputs[this.state.inputs.length - 1], event.target.value], 
@@ -69,11 +85,13 @@ class App extends Component {
   }
 
   decimalHandler(event) {
+    //test case: 3+4... => if there is no '=' in inputs, e.g. inputs: ['3', '+'] and there is no '.' in incompleteInput, e.g.‘4’，then when '.' is pressed, it should be concatted to the end; otherwise if there is already a '.' in the incompleteInput, the newly pressed '.' should be ignored.  
     if(this.state.inputs.indexOf('=') === -1) {
       this.setState({
         incompleteInput: this.state.incompleteInput.indexOf('.') === -1 ? this.state.incompleteInput + (event.target.value + '') : this.state.incompleteInput,
-    })
-  } else {
+      })
+    //test case: 3+4=. => if there is '=' in inputs, e.g. inputs: ['3', '+', '4', '=', '7'], then when a '.' is pressed, it means a new calculation is started. '.' equals to '0.' in Javascript. 
+    } else {
     this.setState({
       inputs: [], 
       incompleteInput: event.target.value,
@@ -82,42 +100,40 @@ class App extends Component {
   }
 
   evalHandler(event) {
-    if(this.state.inputs.length === 0) {
-      if(this.state.incompleteInput !== '') {
-        this.setState({
-          inputs: [this.state.incompleteInput, event.target.value, floatDecimalSolution(eval(this.state.incompleteInput))], 
-          incompleteInput: floatDecimalSolution(eval(this.state.incompleteInput)), 
-        })
-      }
-    } else if(this.state.inputs.length === 1 && isOperator.test(this.state.inputs[0]) === true && this.state.incompleteInput === '') {
-        this.setState({
-          inputs: [], 
-          incompleteInput: '',
-        })
-    } else if(this.state.inputs.length >= 2 && isOperator.test(this.state.inputs[this.state.inputs.length - 1]) === true && this.state.inputs.indexOf('=') === -1) {
+    //test case: .=  => .= is not allowed (the same way as how ./ is not allowed.)
+    if(this.state.inputs.length === 0 && this.state.incompleteInput === '.'){
+    }
+    //test case: 3+5/2==   => if pressing '=' for more than 1 time, all '=' will be ignored except the first. 
+    else if(this.state.inputs.indexOf('=') !== -1){
+    }
+    //test case: 3= => if inputs array is empty, inputs: [], and if incompleteInput is not empty, e.g. incompleteInput: '3', then when '=' is pressed, inputs should become, e.g. ['3' '=' '3']. 
+    else if(this.state.inputs.length === 0 && this.state.incompleteInput !== '') {
+      this.setState({
+        inputs: [this.state.incompleteInput, event.target.value, ' ', floatDecimalSolution(result(this.state.incompleteInput))], 
+        incompleteInput: floatDecimalSolution(result(this.state.incompleteInput)), 
+      })
+    //if inputs: ['3', '+', '4', '-'], 
+    } else if(this.state.inputs.length >= 2 && isOperator.test(this.state.inputs[this.state.inputs.length - 1]) === true) {
+      //test case: 3+4-=  => further if incompleteInput is empty, i.e. '=' is pressed directly, then the '=' should override the '-' and the result will calculated. 
       if(this.state.incompleteInput === '') {
         this.setState({
-          inputs: [this.state.inputs.slice(0, this.state.inputs.length - 1).join(''), event.target.value, floatDecimalSolution(eval(this.state.inputs.slice(0, this.state.inputs.length -1).join('')))], 
-          incompleteInput: floatDecimalSolution(eval(this.state.inputs.slice(0, this.state.inputs.length -1).join(''))), 
+          inputs: [this.state.inputs.slice(0, this.state.inputs.length - 1).join(''), event.target.value, ' ', floatDecimalSolution(result(this.state.inputs.slice(0, this.state.inputs.length -1).join('')))], 
+          incompleteInput: floatDecimalSolution(result(this.state.inputs.slice(0, this.state.inputs.length -1).join(''))), 
         })
+      //test case: 3+4-5=  => further if incompleteInput is not empty, e.g. incompleteInput: '5', then when '=' is pressed, '5' should be concated to the inputs array: ['3', '+', '4', '-', '5'] and then result is calculated.  
       } else {
         this.setState({
-          inputs: [this.state.inputs.join(''), this.state.incompleteInput, event.target.value, floatDecimalSolution(eval(this.state.inputs.concat(this.state.incompleteInput).join('')))], 
-          incompleteInput: floatDecimalSolution(eval(this.state.inputs.concat(this.state.incompleteInput).join(''))), 
+          inputs: [this.state.inputs.join(''), this.state.incompleteInput, event.target.value, ' ', floatDecimalSolution(result(this.state.inputs.concat(this.state.incompleteInput).join('')))], 
+          incompleteInput: floatDecimalSolution(result(this.state.inputs.concat(this.state.incompleteInput).join(''))), 
         })
       }
-    } else if(this.state.inputs.indexOf('=') >= 0) {
-      this.setState({
-        inputs: this.state.inputs, 
-        incompleteInput: this.state.incompleteInput,
-      })
     }
   }
 
   getExtendedState(state) { 
     return {
       formularDisplay: state.inputs.join(''),
-      inputDisplay: state.incompleteInput,
+      inputDisplay: state.warning === '' ? state. incompleteInput : state.warning, 
     };
   }
 
@@ -126,7 +142,9 @@ class App extends Component {
     return (
       <div className='wrap'>
         <div className="calculator">
-          <div id='formula'>{extendedState.formularDisplay}</div>
+          <div id='formular'>
+            <p className='formularDisplay'>{extendedState.formularDisplay}</p>
+          </div>
           <div id='input'>{extendedState.inputDisplay}</div>
           <div className='buttons'>
             <div className='row'>
